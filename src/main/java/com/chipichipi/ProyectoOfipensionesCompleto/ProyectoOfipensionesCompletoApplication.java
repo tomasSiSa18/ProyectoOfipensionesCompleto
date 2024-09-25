4ab087181cf3491cd6a907acdfafcf175520775c
TomasSiSa18 <t.sierras@uniandes.edu.co>
2024-09-24 19:42:15 -0500

    Creacion

---
+++ src/main/java/com/chipichipi/ProyectoOfipensionesCompleto/ProyectoOfipensionesCompletoApplication.java
@@ -0,0 +1,13 @@
+package com.chipichipi.ProyectoOfipensionesCompleto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoOfipensionesCompletoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoOfipensionesCompletoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chipichipi/ProyectoOfipensionesCompleto/ProyectoOfipensionesCompletoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chipichipi.ProyectoOfipensionesCompleto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoOfipensionesCompletoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
